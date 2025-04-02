91dfbd6aa6a069f451f0964b39f29ff5478d4ab5
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-02 13:26:48 +0300

    Create product details page

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -16,7 +16,7 @@ const Header = () => {
               height={48}
               priority={true}
             />
-            <span className='hidden lg:block font-bold text-2xl ml-3'>
+            <span className='hidden md:block font-bold text-2xl ml-3'>
               {APP_NAME}
             </span>
           </Link>
--- src/components/shared/product/product-card.tsx
+++ src/components/shared/product/product-card.tsx
@@ -3,12 +3,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import ProductPrice from '@/components/shared/product/product-price'
 import { Product } from '@/types'
+import { Button } from '@/components/ui/button'
 
 const ProductCard = ({ product } : {
   product : Product
 }) => {
   return (
-    <Card className='w-full max-w-[300px] mx-auto'>
+    <Card className='w-full max-w-[300px] mx-auto pt-0'>
       <CardHeader className='p-0 items-center'>
         <Link href={`/product/${product.slug}`}>
           <Image className='rounded-t-md w-full aspect-square object-cover' src={product.images[0]} alt={product.name} height={300} width={300} priority={true} />
