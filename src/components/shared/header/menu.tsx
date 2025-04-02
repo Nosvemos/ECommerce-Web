809f9754a25340a52faa05cdd9d2562682951639
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-02 16:29:31 +0300

    Create user button and sign-out button

--- src/components/shared/header/menu.tsx
+++ src/components/shared/header/menu.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { Menu, ShoppingCart, UserIcon } from 'lucide-react'
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import UserButton from '@/components/shared/header/user-button'
 
 const ResponsiveMenu = () => {
   return (
@@ -14,11 +15,7 @@ const ResponsiveMenu = () => {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href='/sign-in'>
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton/>
       </nav>
       <nav className='md:hidden'>
         <Sheet>
@@ -35,11 +32,7 @@ const ResponsiveMenu = () => {
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href='/sign-in'>
-                <UserIcon /> Sign In
-              </Link>
-            </Button>
+            <UserButton/>
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
---
+++ src/components/shared/header/user-button.tsx
@@ -0,0 +1,63 @@
+import { auth } from '@/auth'
+import Link from 'next/link'
+import { LogOut, UserIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent, DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import Form from 'next/form'
+import { signOutUser } from '@/lib/actions/user.actions'
+
+const UserButton = async () => {
+  const session = await auth();
+  if (!session) {
+    return (
+      <Button asChild>
+        <Link href='/sign-in'>
+          <UserIcon /> Sign In
+        </Link>
+      </Button>
+    )
+  }
+
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? 'U';
+
+  return (
+    <div className='flex gap-2 items-center'>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className='flex items-center'>
+            <Button variant='ghost' className='relative size-8 rounded-full ml-2 flex items-center justify-center bg-gray-200'>
+              {firstInitial}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56' align='end' forceMount>
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <span className='text-sm font-medium leading-none'>
+                {session.user?.name}
+              </span>
+              <span className='text-sm text-muted-foreground leading-none'>
+                {session.user?.email}
+              </span>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem className='p-0 mb-1'>
+            <Form action={signOutUser} className='w-full'>
+              <Button className='py-4 px-2 h-4 justify-start w-full' variant='ghost'>
+                <LogOut/>
+                <span>Sign Out</span>
+              </Button>
+            </Form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
+
+export default UserButton
