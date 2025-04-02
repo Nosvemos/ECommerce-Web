50c6d28682bf31856bdc7d6da6b94f45fa3c533d
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-02 12:31:18 +0300

    Create validators with Zod

--- src/components/shared/product/product-list.tsx
+++ src/components/shared/product/product-list.tsx
@@ -1,7 +1,8 @@
 import ProductCard from '@/components/shared/product/product-card'
+import { Product } from '@/types'
 
 const ProductList = ({ data, title, limit } : {
-  data: any, // Only for now
+  data: Product[], // Only for now
   title?: string,
   limit?: number
 }) => {
@@ -12,7 +13,7 @@ const ProductList = ({ data, title, limit } : {
       <h2 className='h2-bold mb-4 text-center sm:text-left'>{title}</h2>
       { limitedData.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.slug} product={product}></ProductCard>
           ))}
         </div>
