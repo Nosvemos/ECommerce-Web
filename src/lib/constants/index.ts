46b68b76b81597df6bffb3223b58e8df3ed2e546
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-02 11:54:58 +0300

    Load products from database

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "E-Commerce Web";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern e-commerce web application built with Next.js";
 export const SERVER_URL = process.env.NEXT_PUBLIC_APP_SERVER_URL || "http://localhost:3000";
+
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
