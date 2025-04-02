004f9b45e9d0f8c92d73a202a61252276ab3d52e
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-02 15:41:18 +0300

    Create sign-in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from 'next'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import Link from 'next/link'
+import Image from 'next/image'
+import { APP_NAME } from '@/lib/constants'
+import CredentialsSignInForm from '@/components/credentials-sign-in-form'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'Sign In'
+}
+
+const SignInPage = async() => {
+  const session = await auth();
+  if (session) redirect('/');
+
+  return (
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          <Link href='/' className='flex-center'>
+            <Image src='/images/logo.svg' width={100} height={100} alt={`${APP_NAME} Logo`} priority={true}></Image>
+          </Link>
+          <CardTitle className='text-center'>Sign In</CardTitle>
+          <CardDescription className='text-center'>Sign in to your account</CardDescription>
+          <CardContent className='space-y-4'>
+            <CredentialsSignInForm/>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </div>
+  )
+}
+
+export default SignInPage
---
+++ src/components/credentials-sign-in-form.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { SIGN_IN_DEFAULT_VALUES } from '@/lib/constants'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { useActionState } from 'react'
+import { signInWithCredentials } from '@/lib/actions/user.actions'
+import Form from 'next/form'
+import { useFormStatus } from 'react-dom'
+import { Loader2Icon } from 'lucide-react'
+
+const CredentialsSignInForm = () => {
+  const initialState = {
+    success: false,
+    message: '',
+  };
+  const [state, formAction] = useActionState(signInWithCredentials, initialState);
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className='w-full'>
+        { pending ? (
+          <>
+            <Loader2Icon className='animate-spin'/>
+            <p>Signing In...</p>
+          </>
+        ) : (
+          'Sign In'
+        )}
+      </Button>
+    )
+  }
+
+  return (
+    <Form action={formAction}>
+      <div className='space-y-6'>
+        <div>
+          <Label htmlFor='email'>Email</Label>
+          <Input id='email' name='email' type='email' autoComplete='email' defaultValue={SIGN_IN_DEFAULT_VALUES.email} required />
+        </div>
+        <div>
+          <Label htmlFor='password'>Password</Label>
+          <Input id='password' name='password' type='password' autoComplete='password' defaultValue={SIGN_IN_DEFAULT_VALUES.password} required />
+        </div>
+        <div>
+          <SignInButton/>
+        </div>
+        { state && !state.success && state.message && (
+          <div className='text-center text-destructive text-sm'>
+            {state.message}
+          </div>
+        )}
+        <div className='text-sm text-center text-muted-foreground'>
+          Don't have an account?{' '}
+          <Link href='/sign-up' target='_self' className='link'>Sign Up</Link>
+        </div>
+      </div>
+    </Form>
+  )
+}
+
+export default CredentialsSignInForm
