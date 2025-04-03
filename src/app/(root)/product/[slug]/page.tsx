3078fb9701ad31ae895862a5622822f09974f118
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-03 12:43:10 +0300

    Fix responsive design on product detail page

--- src/app/(root)/product/[slug]/page.tsx
+++ src/app/(root)/product/[slug]/page.tsx
@@ -15,55 +15,52 @@ const ProductDetailsPage = async(props: {
   if (!product) notFound();
 
   return (
-    <>
-      <section>
+    <section>
+      <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5">
         {/* Images Column */}
-        <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5">
-          {/* Images Column */}
-          <div className="col-span-2">
-            <ProductImages images={product.images} />
-          </div>
-          <div className='col-span-2 p-5'>
-            <div className='flex flex-col gap-6'>
-              <p>{product.brand} {product.category}</p>
-              <h3 className='h3-bold'>{product.name}</h3>
-              <p>{product.rating} of {product.numReviews} Reviews</p>
-              <div className='flex flex-col md:flex-row md:items-center gap-3'>
-                <ProductPrice value={Number(product.price)} className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2' />
-              </div>
-            </div>
-            <div className='mt-10'>
-              <p className='font-semibold'>Description</p>
-              <p>{product.description}</p>
+        <div className="md:col-span-2 col-span-2">
+          <ProductImages images={product.images} />
+        </div>
+        <div className='col-span-2 p-5'>
+          <div className='flex flex-col gap-6'>
+            <p>{product.brand} {product.category}</p>
+            <h3 className='h3-bold'>{product.name}</h3>
+            <p>{product.rating} of {product.numReviews} Reviews</p>
+            <div className='flex flex-col md:flex-row md:items-center gap-3'>
+              <ProductPrice value={Number(product.price)} className='w-24 rounded-full bg-green-100 text-green-700 px-5 py-2' />
             </div>
           </div>
-          {/* Action Column */}
-          <div className='md:col-start-3 md:col-span-2 lg:col-span-1 justify-end'>
-            <Card className='bg-card/30'>
-              <CardContent>
-                <div className='mb-2 flex justify-between'>
-                  <p>Price</p>
-                  <ProductPrice value={Number(product.price)} />
-                </div>
-                <div className='mb-2 flex justify-between'>
-                  <p>Status</p>
-                  {product.stock > 0 ? (
-                    <Badge variant='outline'>In Stock</Badge>
-                  ) : (
-                    <Badge variant='destructive'>Out of Stock</Badge>
-                  )}
-                </div>
-                {product.stock > 0 && (
-                  <div className='flex-center'>
-                    <Button className='w-full mt-2'>Add to Cart</Button>
-                  </div>
-                )}
-              </CardContent>
-            </Card>
+          <div className='mt-10'>
+            <p className='font-semibold'>Description</p>
+            <p>{product.description}</p>
           </div>
         </div>
-      </section>
-    </>
+        {/* Action Column */}
+        <div className='col-span-1 md:col-span-4 lg:col-span-1 mt-0 md:mt-2'>
+          <Card className='bg-card/30'>
+            <CardContent>
+              <div className='mb-2 flex justify-between'>
+                <p>Price</p>
+                <ProductPrice value={Number(product.price)} />
+              </div>
+              <div className='mb-2 flex justify-between'>
+                <p>Status</p>
+                {product.stock > 0 ? (
+                  <Badge variant='outline'>In Stock</Badge>
+                ) : (
+                  <Badge variant='destructive'>Out of Stock</Badge>
+                )}
+              </div>
+              {product.stock > 0 && (
+                <div className='flex-center'>
+                  <Button className='w-full mt-2'>Add to Cart</Button>
+                </div>
+              )}
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </section>
   )
 }
 
