25ad59e7ca2a62bbd0e5c16893e7cf01b4d6a0c2
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-05 20:22:47 +0300

    Generate PayPal sandbox API access token

---
+++ src/lib/paypal.ts
@@ -0,0 +1,26 @@
+const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
+
+export const paypal = {};
+
+// Generate access token
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`, 'base64');
+
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+      Authorization: `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  });
+
+  if (response.ok) {
+    const { access_token } = await response.json();
+    return access_token;
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
