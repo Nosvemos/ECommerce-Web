fe3ea62be6a6542e304756dafa74ca1e9ccadccf
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-13 14:12:25 +0300

    Create category drawer

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { APP_NAME } from '@/lib/constants'
 import Menu from '@/components/shared/header/menu'
-import { Input } from '@/components/ui/input'
 import MainNav from '@/components/shared/main-nav'
 import AdminSearch from '@/components/admin/admin-search'
 
