18df79f4424c8c384ed62872bee851a96f08954c
Samet Ozturk <53067521+Nosvemos@users.noreply.github.com>
2025-04-14 17:12:15 +0300

    Create Stripe payment component and form

--- src/components/stripe-payment.tsx
+++ src/components/stripe-payment.tsx
@@ -1,10 +1,47 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import StripeForm from '@/components/forms/stripe-form'
+import { useTheme } from 'next-themes'
+
 const StripePayment = ({ priceInCents, orderId, clientSecret } : {
   priceInCents: number,
   orderId: string,
   clientSecret: string
 }) => {
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+  );
+
+  const { theme, systemTheme } = useTheme();
+
   return (
-    <div>Stripe Payment</div>
+    <Elements
+      options={{
+        clientSecret,
+        appearance: {
+          theme:
+            theme === 'dark'
+              ? 'night'
+              : theme === 'light'
+                ? 'stripe'
+                : systemTheme === 'light'
+                  ? 'stripe'
+                  : 'night',
+          variables: {
+            colorBackground: theme === 'dark'
+              ? '#0e172a'
+              : theme === 'light'
+                ? ''
+                : systemTheme === 'light'
+                  ? ''
+                  : '#0e172a',
+          }
+        },
+      }}
+      stripe={stripePromise}
+    >
+      <StripeForm priceInCents={priceInCents} orderId={orderId} />
+    </Elements>
   )
 }
 
